209242a41d9c1c2d71545e5971673146f5886dba
Jshhhh <36183898+Jshhhh@users.noreply.github.com>
2018-11-02 15:34:41 -0700

    Add comments for future fxns

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,23 +1,20 @@
 const ADD_PLAYER = 'ADD_PLAYER';
 const CHANGE_NAME = 'CHANGE_NAME';
 
-const changeName = name => {
-    return {
-        type: CHANGE_NAME,
-        payload: name
-    }
-}
+//toggle cell from '', 'X', 'O'
+//add check for win condition function
 
-const addName = name => {
-    return {
-        type: ADD_PLAYER,
-        payload: name
-    }
-}
+// can implement changeName later
+// const changeName = name => {
+//     return {
+//         type: CHANGE_NAME,
+//         payload: name
+//     }
+// }
 
-module.exports = {
-    changeName,
-    CHANGE_NAME,
-    ADD_PLAYER,
-    addName
-};
+// module.exports = {
+//     changeName,
+//     CHANGE_NAME,
+//     ADD_PLAYER,
+//     addName
+// };
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,11 +1,15 @@
-import { ADD_PLAYER, CHANGE_NAME } from '../actions/actions';
+// import { ADD_PLAYER, CHANGE_NAME } from '../actions/actions';
 
+
+//add a board grid
+//win condition boolean
 const initialState = {
     playerName: '',
     players: ['Colonel Mustard', 'Prof Plum', 'Miss Scarlet', 'Mr. Green', 'Mrs. Peacock', 'Mrs. White'],
     weapons: ['Knife','Revolver','Wrench','Candlestick','Lead Piping','Rope'],
     rooms: ['Kitchen','Ballroom','Conservatory','Dining Room','Lounge','Hall','Study','Billiard Room', 'Library'],
 };
+
 
 const rootReducer = (state=initialState, action) => {
     switch (action.type) {
